716dd2ba5770e50e5099702c2a19a4d51c4fe366
Ayomide57 <39005838+Ayomide57@users.noreply.github.com>
2018-05-10 09:27:37 +0100

    change save
    change save to submit

--- backend/views/email-template/_form.php
+++ backend/views/email-template/_form.php
@@ -29,7 +29,7 @@ use dosamigos\tinymce\TinyMce;
     ]);?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
